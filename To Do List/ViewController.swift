8dc246c2a45cf5ef69cc3e6e3c42c3b645cb1836
default <ykim@fa05956kyoung-tae.bc.edu>
2019-02-10 00:50:34 -0500

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Kim, Young-Tae on 2/10/19.
+//  Copyright © 2019 David Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
